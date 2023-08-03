173d1071c20250d1529d807d0a0ecb3f909fa095
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-08-03 23:54:21 +0300

    buttons now

--- ToDoList/frontend/components/Navbar.tsx
+++ ToDoList/frontend/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button"
 import { ChevronDown } from "lucide-react"
+import { Button } from "./ui/button";
 
 import {
     NavigationMenu,
@@ -15,31 +15,20 @@ import {
 export function Navbar() {
     return (
 <NavigationMenu className="w-screen bg-neutral-900 shadow-xl h-12 grid grid-cols-[1fr_120px] md:h-16 md:grid-cols-[1fr_180px]">
-  <NavigationMenuList className="flex h-full items-center max-h-12 md:max-h-16">
-    <NavigationMenuItem className="">
-      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
-    </NavigationMenuItem>
-    <NavigationMenuItem className="">
-      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
-    </NavigationMenuItem>
-    <NavigationMenuItem className="">
-      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
-    </NavigationMenuItem>
-    <NavigationMenuItem className="">
-      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
-    </NavigationMenuItem>
-    <NavigationMenuItem>
-      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
+  <NavigationMenuList className="flex h-full items-center max-h-12 md:max-h-16 flex-grow">
+    <NavigationMenuItem className=" flex-grow flex-row flex-nowrap items-center">
+      <Button className="w-1/8 bg-neutral-800 text-white rounded py-1 px-1 m-1 text-[10px] md:text-sm">sometext</Button>
+      <Button className=" bg-neutral-800 text-white rounded py-1 px-1 m-1 text-md">+</Button>
     </NavigationMenuItem>
   </NavigationMenuList>
   <NavigationMenuList className="">
     <NavigationMenuItem className="">
-      <NavigationMenuTrigger className=" bg-black text-white  h-12 md:h-16 p-1 w-full ">
-        <div  className="flex justify-between items-center">
+      <NavigationMenuTrigger className="">
+        <Button  className="flex justify-between items-center rounded-md bg-neutral-800 text-white  h-12 md:h-16 p-1 w-full ">
           <ChevronDown className="h-8 w-8 md:h-10 md:w-10" />
           <p className="px-1 text-[10px] md:text-sm">Username98</p> {/*10 characters max */}
           <img className="h-8 w-8 md:h-12 md:w-12 " src="https://dummyimage.com/250/ffffff/000000" alt="placeholder " />
-        </div>
+        </Button>
       </NavigationMenuTrigger>
       <NavigationMenuContent>
         <NavigationMenuLink><div className=" bg-red-900 text-white  ">TODOLIST!</div></NavigationMenuLink>
