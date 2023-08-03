56419aa129ecdd8694cef57e6a80ba0e38201520
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-08-03 22:54:05 +0300

    removed tailwind buttons, failed to work with them

--- ToDoList/frontend/components/Navbar.tsx
+++ ToDoList/frontend/components/Navbar.tsx
@@ -14,34 +14,44 @@ import {
 
 export function Navbar() {
     return (
-<NavigationMenu className=" h-16 bg-gray-900 shadow-xl w-screen flex justify-between p-0.5">
-  <NavigationMenuList className="border-4 flex ">
+<NavigationMenu className="w-screen bg-neutral-900 shadow-xl h-12 grid grid-cols-[1fr_120px] md:h-16 md:grid-cols-[1fr_180px]">
+  <NavigationMenuList className="flex h-full items-center max-h-12 md:max-h-16">
     <NavigationMenuItem className="">
-    <Button className=" bg-black text-white shadow-xl">TODOLIST!</Button>
+      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
+    </NavigationMenuItem>
+    <NavigationMenuItem className="">
+      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
+    </NavigationMenuItem>
+    <NavigationMenuItem className="">
+      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
+    </NavigationMenuItem>
+    <NavigationMenuItem className="">
+      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
     </NavigationMenuItem>
     <NavigationMenuItem>
-      <Button className=" bg-black text-white shadow-xl">TODOLIST!</Button>
+      <div className=" bg-neutral-800 text-white rounded py-2 px-1 m-1 text-[10px] md:text-sm">TODOLIST!</div>
     </NavigationMenuItem>
   </NavigationMenuList>
   <NavigationMenuList className="">
     <NavigationMenuItem className="">
-      <NavigationMenuTrigger>
-        <Button  className=" bg-black text-white shadow-xl h-full">
-        <ChevronDown className="h-10 w-6 " />
-        <p>PROFILE NAME</p>
-        </Button>
+      <NavigationMenuTrigger className=" bg-black text-white  h-12 md:h-16 p-1 w-full ">
+        <div  className="flex justify-between items-center">
+          <ChevronDown className="h-8 w-8 md:h-10 md:w-10" />
+          <p className="px-1 text-[10px] md:text-sm">Username98</p> {/*10 characters max */}
+          <img className="h-8 w-8 md:h-12 md:w-12 " src="https://dummyimage.com/250/ffffff/000000" alt="placeholder " />
+        </div>
       </NavigationMenuTrigger>
       <NavigationMenuContent>
-        <NavigationMenuLink><Button className=" bg-black text-white shadow-xl  ">TODOLIST!</Button></NavigationMenuLink>
+        <NavigationMenuLink><div className=" bg-red-900 text-white  ">TODOLIST!</div></NavigationMenuLink>
       </NavigationMenuContent>
       <NavigationMenuContent>
-        <NavigationMenuLink><Button className=" bg-black text-white shadow-xl  ">TODOLIST!</Button></NavigationMenuLink>
+        <NavigationMenuLink><div className=" bg-black text-white  ">TODOLIST!</div></NavigationMenuLink>
       </NavigationMenuContent>
       <NavigationMenuContent>
-        <NavigationMenuLink><Button className=" bg-black text-white shadow-xl  ">TODOLIST!</Button></NavigationMenuLink>
+        <NavigationMenuLink><div className=" bg-yellow-900 text-white  ">TODOLIST!</div></NavigationMenuLink>
       </NavigationMenuContent>
       <NavigationMenuContent>
-        <NavigationMenuLink><Button className=" bg-black text-white shadow-xl  ">TODOLIST!</Button></NavigationMenuLink>
+        <NavigationMenuLink><div className=" bg-purple-900 text-white  ">TODOLIST!</div></NavigationMenuLink>
       </NavigationMenuContent>
     </NavigationMenuItem> 
   </NavigationMenuList>
