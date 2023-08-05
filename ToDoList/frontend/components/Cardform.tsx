be4b39099cb10ff8b07a4ab5f057001a81004e4e
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-08-05 12:20:08 +0300

    home page card, functions added

--- ToDoList/frontend/components/Cardform.tsx
+++ ToDoList/frontend/components/Cardform.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -10,13 +12,58 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { addUser, checkUser, convertFileToBase64 } from "@/lib/server-utils";
+import { useRouter } from "next/navigation";
 
 export function Cardfrom() {
+  const router = useRouter();
+  const [signupData, setSignupData] = useState({
+    username: "",
+    password: "",
+    img: null as File | null,
+  });
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+  const [loginError, setLoginError] = useState(false);
+  const [signupMessage, setSignupMessage] = useState<string | null>(null);
+
+  const handleLogin = async () => {
+    const check_user = await checkUser(loginData.username, loginData.password);
+    if (check_user) {
+      router.push(`/todolist/${loginData.username}`);
+      setLoginData({
+        username: "",
+        password: "",
+      });
+    } else {
+      setLoginError(true);
+    }
+  };
+
+  const handleSignup = async () => {
+    const responseMessage = await addUser(
+      signupData.username,
+      signupData.password,
+      signupData.img
+    );
+    if (responseMessage === "User created successfully") {
+      router.push(`/todolist/${signupData.username}`);
+      setSignupData({
+        username: "",
+        password: "",
+        img: null as File | null,
+      });
+    }
+    setSignupMessage(responseMessage);
+  };
+
   return (
     <Tabs
       defaultValue="login"
       className="flex flex-col flex-grow w-full h-full xl:w-1/2 xl:h-1/2"
-      id={"1"}
     >
       <TabsList className="self-start">
         <TabsTrigger
@@ -59,6 +106,12 @@ export function Cardfrom() {
                 id="username"
                 placeholder="Username"
                 className="h-3/5 md:h-2/5"
+                onChange={(e) =>
+                  setLoginData((prevData) => ({
+                    ...prevData,
+                    username: e.target.value,
+                  }))
+                }
               />
             </div>
             <div className="h-2/5">
@@ -70,16 +123,28 @@ export function Cardfrom() {
                 id="password"
                 placeholder="Password"
                 className="h-3/5 md:h-2/5"
+                onChange={(e) =>
+                  setLoginData((prevData) => ({
+                    ...prevData,
+                    password: e.target.value,
+                  }))
+                }
               />
             </div>
           </CardContent>
-          <CardFooter className="flex justify-center w-full p-0 mt-10 md:mt-5 md:h-1/5">
+          <CardFooter className="flex flex-col justify-center w-full p-0 mt-10 space-y-2 md:mt-5 md:h-1/5">
             <Button
               variant="outline"
               className="w-3/4 shadow-md hover:bg-orange-400 md:h-1/3 md:text-lg xl:text-2xl"
+              onClick={handleLogin}
             >
               Log In
             </Button>
+            {loginError && (
+              <span className="text-sm tracking-tighter text-gray-500 md:text-base">
+                Failed to Login &#9940;
+              </span>
+            )}
           </CardFooter>
         </Card>
       </TabsContent>
@@ -91,12 +156,12 @@ export function Cardfrom() {
         id="radix-:R1mcq:-trigger-signup"
       >
         <Card className="flex flex-col items-center w-full h-full p-5 ">
-          <CardHeader>
+          <CardHeader className="p-2 md:p-5 xl:p-14">
             <CardTitle className="font-bold leading-10 text-center md:text-5xl md:leading-[5rem]">
               Sign Up To Create A New Account!
             </CardTitle>
           </CardHeader>
-          <CardContent className="w-full space-y-4 md:space-y-8 md:h-2/5">
+          <CardContent className="w-full space-y-1 md:space-y-8 md:h-2/5">
             <div className="h-2/5">
               <Label htmlFor="username" className="text-xl ">
                 Create A Username
@@ -106,6 +171,13 @@ export function Cardfrom() {
                 id="username"
                 placeholder="Username"
                 className="h-2/5"
+                required
+                onChange={(e) =>
+                  setSignupData((prevData) => ({
+                    ...prevData,
+                    username: e.target.value,
+                  }))
+                }
               />
               <span className="text-sm tracking-tighter text-gray-500 md:text-base">
                 This is your public display name.
@@ -120,6 +192,13 @@ export function Cardfrom() {
                 id="password"
                 placeholder="Password"
                 className="h-2/5"
+                required
+                onChange={(e) =>
+                  setSignupData((prevData) => ({
+                    ...prevData,
+                    password: e.target.value,
+                  }))
+                }
               />
               <span className="text-sm tracking-tighter text-gray-500 md:text-base">
                 Enter a password to secure your acount
@@ -129,16 +208,38 @@ export function Cardfrom() {
               <Label htmlFor="picture" className="text-xl ">
                 Picture
               </Label>
-              <Input id="picture" type="file" className="h-2/5" />
+              <Input
+                id="picture"
+                type="file"
+                className="h-2/5"
+                onChange={async (e) => {
+                  const img = e.target.files ? e.target.files[0] : null;
+                  const imgBase64 = img ? await convertFileToBase64(img) : null;
+
+                  setSignupData((prevData) => ({
+                    ...prevData,
+                    img: imgBase64 as File | null,
+                  }));
+                }}
+              />
+              <span className="text-sm tracking-tighter text-gray-500 md:text-base">
+                Insert a profile pic if you wish &#128515;
+              </span>
             </div>
           </CardContent>
-          <CardFooter className="flex justify-center w-full p-0 mt-10 md:mt-5 md:h-1/5">
+          <CardFooter className="flex flex-col justify-center w-full p-0 mt-10 space-y-1 md:mt-5 md:h-1/5">
             <Button
               variant="outline"
-              className="w-3/4 shadow-md hover:bg-orange-400 md:h-1/3 md:text-lg xl:text-2xl"
+              className="w-3/4 mt-5 shadow-md md:mt-52 xl:mt-64 hover:bg-orange-400 md:h-1/3 md:text-lg xl:text-2xl"
+              onClick={handleSignup}
             >
               Sign Up
             </Button>
+            {signupMessage && (
+              <span className="text-sm tracking-tighter text-blue-400 md:text-base">
+                {signupMessage} &#10071;
+              </span>
+            )}
           </CardFooter>
         </Card>
       </TabsContent>
