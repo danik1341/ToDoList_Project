e8a83f92c95d12d3916665729fd7e67c77943d0e
Gabriel Dischon <gabrielgdischon@gmail.com>
2023-08-06 15:14:19 +0300

    first commit

--- ToDoList/frontend/components/Task.tsx
+++ ToDoList/frontend/components/Task.tsx
@@ -1,7 +1,7 @@
 import { Button } from "./ui/button";
 export function Task() {
     return (
-<div className=" md:mx-20 lg:mx-40 xl:mx-60 ">    
+<div className=" md:mx-20 lg:mx-40 xl:mx-60 ">
     <h1 className=' pt-1 text-4xl text-center '>ToDoList Title</h1>
     <div className=" flex flex-row rounded-2xl border-r-2 border-b-2 border-neutral-400 shadow-2xl min-h-[400px] mb-4">
         <aside className=" flex flex-col md:flex-row bg-gray-200 rounded-l-2xl">
@@ -40,7 +40,40 @@ export function Task() {
             </ul>
         </aside>
     </div>
+    <div className=" md:text-xl rounded-2xl border-r-2 border-b-2 border-neutral-400 shadow-lg min-h-[600px] flex flex-col items-center pb-8 mb-4">
+        <h1 className="mb-8 text-4xl">Edit Your Task</h1>
+        <div className=" flex flex-wrap justify-center space-y-4">
 
+            <label className="flex justify-between w-[90%] items-center">
+                New Title: <input className="w-3/4 border-[1px] border-black rounded-lg p-1 md:px-1" name="title" />
+            </label>
+            <label className="flex justify-between w-[90%] items-center">
+                New Category: <input className="w-3/4 border-[1px] border-black rounded-lg p-1" name="category" />
+            </label>
+            <label className="flex justify-between w-[90%] items-center">
+                New Due time:<input className="w-3/4 border-[1px] border-black rounded-lg p-1" type="datetime-local"></input>
+            </label>
+            <label className=" w-[90%] ">
+                Reset Creation Time: <input type="checkbox" name="myCheckbox" />
+            </label>            
+            <label className=" w-[90%] ">
+                Clean Old Images: <input type="checkbox" name="myCheckbox" />
+            </label>            
+        </div>
+        <br />
+        <div className="w-full flex flex-col items-center">
+            <label className="w-[90%] flex items-center">
+                Add Images: <input type="image" src="#" alt="" width="48" height="48"/>
+            </label>
+            <label className="w-[90%] text-center">
+                Description: <textarea className="w-full h-80 border-[1px] border-black rounded-lg p-1" name="description" />
+            </label>
+        </div>
+        <div>
+            <input className=" bg-blue-300 text-white focus:bg-blue-600 hover:bg-orange-500 rounded-lg px-4 py-2 m-4" type="submit" value="Submit"/>
+            <input className=" bg-blue-300 text-white focus:bg-blue-600 hover:bg-orange-500 rounded-lg px-4 py-2 m-4" type="reset"/>
+        </div>
+    </div>
 </div>
     );
 }
